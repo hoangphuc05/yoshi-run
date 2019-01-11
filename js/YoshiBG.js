f9f9585d44954444aee08ca44b0a821fcadf9f68
Phuc H Cai <pcai22@my.whitworth.edu>
2019-01-11 13:27:07 -0800

    working resources and background

--- js/YoshiBG.js
+++ js/YoshiBG.js
@@ -1,8 +1,21 @@
+
 (function ($) {
 
     var spot = 0;
-    var yoshispeed = 12;
+    var yoshispeed = -40;             //orig : var yoshispeed = -40;
+
+    var $yoshi_right = $('<img/>');
+    $yoshi_right[0].src = "img/yoshirunright.gif";
+  
+  
+    var $yoshi_left = $('<img/>');
+    $yoshi_left[0].src = "img/yoshirunleft.gif";
+  
+  
+    $('div#Yoshi').empty().append($yoshi_right);
     var $yoshibg = $("#YoshiBG_furthestBack");
+
+
 
     var requestAnimFrame = (function() {
         if (window.requestAnimationFrame) return window.requestAnimationFrame;
@@ -13,9 +26,22 @@
         else return function(callback, element) {
             window.setTimeout(callback, element);
         };
-
-
     })();
+
+
+
+    $("body" ).click(function() {
+        yoshispeed *= -1.0;
+                                   
+        if ( yoshispeed > 0 )    
+        {
+            $('div#Yoshi').empty().append($yoshi_left);
+        }                                                                //orig: if (yoshispeed > 0) {$('div#Yoshi').empty().append($yoshi_left); }
+                
+        else
+            $('div#Yoshi').empty().append($yoshi_right);
+        $('div#Yoshi').show();
+      });
 
     function draw () {
 
@@ -25,9 +51,9 @@
         $('#YoshiBG_mid').css('background-position', (spot * (1.0/6.0)) );
         $('#YoshiBG_frontmost').css('background-position', (spot * (1.0/3.0)) );
        
-        spot = spot - yoshispeed;
+        spot = spot + yoshispeed;
 
-        if (spot* (1.0/12.0) < -$yoshibg.width() ) {
+        if (spot* (1.0/12.0) <= -$yoshibg.width() || spot*(1.0/12.0) >= $yoshibg.width()) {
             spot = 0;
         }
 
